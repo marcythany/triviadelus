74e1a5c054a0f314fa5193954f8ad13270c88ade
Marcy Sobral <marcythany@gmail.com>
2024-06-11 01:29:39 -0300

    feat: Semantic refactoring
    fixed some tag to turn the code more semantic.

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -12,43 +12,41 @@ export default function LayoutDefault({
 }) {
   return (
     <div className="flex flex-col gap-2 max-w-5xl m-auto">
-      <Sidebar>
+      <Navbar>
         <Logo />
         <Link href="/" className="flex items-center" >Welcome</Link>
         <Link href="/trivia" className="flex items-center">Trivia</Link>
-      </Sidebar>
+      </Navbar>
       <Content>{children}</Content>
     </div>
   );
 }
 
-function Sidebar({ children }: { children: React.ReactNode }) {
+function Navbar({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      id="sidebar"
+    <nav
+      id="navbar"
       className="px-5 gap-5 flex h-[5rem]"
     >
       {children}
-    </div>
+    </nav>
   );
 }
 
 function Content({ children }: { children: React.ReactNode }) {
   return (
-    <div id="page-container">
-      <div id="page-content" className="">
+      <main id="page-content" className="">
         {children}
-      </div>
-    </div>
+      </main>
   );
 }
 
 function Logo() {
   return (
-    <div className="flex items-center">
+    <picture className="flex items-center">
       <a href="/">
         <img src={logoUrl} height={64} width={64} alt="logo" />
       </a>
-    </div>
+    </picture>
   );
 }
